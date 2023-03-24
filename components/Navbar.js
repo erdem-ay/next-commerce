bbe5bcf0279eef7b54c3ffb71709fae5fe3303c1
Erdem AY <eirdemmoon@gmail.com>
2023-03-24 21:57:19 +0300

    registerPage

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,29 @@
 import Link from "next/link";
 import Search from "./Search";
+import { BsPerson } from "react-icons/bs";
 
 const Navbar = () => {
-
-
   return (
     <nav className="fixed h-[8vh] bg-gray-800 flex justify-around w-full">
       <div className="max-w-7xl mx-auto px-4  sm:px-6 lg:px-8 w-full">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-          <Link href="/">
-            <img className="h-8 w-8" src="/logo.svg" alt="Logo" />
-          </Link>
+            <Link href="/">
+              <img className="h-8 w-8" src="/logo.svg" alt="Logo" />
+            </Link>
           </div>
           <div className="flex items-center">
+            <Link href="/login">
+              <BsPerson className="text-xl text-white" />
+            </Link>
             <div className="mr-4 space-x-4">
-    
               <Link href="/products">
                 <i className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                   Products
                 </i>
               </Link>
             </div>
-            <Search/>
+            <Search />
           </div>
         </div>
       </div>
