0772ebc4cb9cec3bda0807b31e9d6bf8e0cb837f
Erdem AY <eirdemmoon@gmail.com>
2023-04-11 17:57:00 +0300

    loading gif add

--- components/Products.js
+++ components/Products.js
@@ -1,5 +1,7 @@
 import { useCallApi } from "@/utils/api";
 import ProductCard from "./ProductCard";
+import loadingGif from "../assets/loading.gif";
+import Image from "next/image";
 
 const Products = () => {
   const { data, error, loading } = useCallApi(
@@ -11,7 +13,9 @@ const Products = () => {
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 m-2">
         {loading ? (
-          <>Loading..</>
+          <div className="flex justify-center items-center h-screen w-screen">
+            <Image src={loadingGif} alt="Gif" width={500} height={500} />
+          </div>
         ) : (
           data?.map((item, index) => <ProductCard key={index} product={item} />)
         )}
